8129bd15b57c3b4af83a1bd286ec124ef839ab39
andreselianor <113841548+andreselianor@users.noreply.github.com>
2024-09-24 11:55:46 +0200

    update DAA
    Activity 4.4.2

--- UNIT2/Activity442/rsc/Main.java
+++ UNIT2/Activity442/rsc/Main.java
@@ -1,32 +1,16 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Ejercicio 4.4.2");
+        System.out.println("Reconocer el formato de imagen");
 
-        Scanner scanner = new Scanner(System.in);
-
-        try {
-            FileInputStream fileStream = new FileInputStream("images/imagenJPG.jpg");
-            byte[] byte6 = new byte[6];
-            int test = fileStream.read(byte6);
-            int hexadecimal = 0x30;
-            System.out.println(test);
-        } catch (FileNotFoundException e) {
-            System.out.println("El archivo no existe");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-
-
+        App application = new App();
+        application.getFirstBytes();
     }
 }
 
 /*
+    EXERCISE 4.4.2
+    --------------
     Write a program to detect the format of an image file. To detect the type of
     the file, the first bytes should be readed. Here you have a table with the header
     bytes for every format:
