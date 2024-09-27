7dfe8f09d15e265ae5fc1f9c939504dc21899313
andreselianor <113841548+andreselianor@users.noreply.github.com>
2024-09-27 13:33:50 +0200

    update TestsUnity 4.4.2

--- UNIT2/Activity442/TestsUnit/TestsApp.java
+++ UNIT2/Activity442/TestsUnit/TestsApp.java
@@ -2,10 +2,34 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestsApp {
+    App application = new App();
+
     @Test
-    public void UnitTest1(){
-        App application = new App();
-        Assertions.assertEquals(application.imageFormatDetection("images/imagenBMP.bmp"),FileType.JPG);
+    public void unitTest1() {
+        Assertions.assertEquals(application.imageFormatDetection("images/imagenBMP.bmp"), FileType.BMP);
     }
 
+    @Test
+    public void unitTest2() {
+        Assertions.assertEquals(application.imageFormatDetection("images/imagenJPG.jpg"), FileType.JPG);
+    }
+
+    @Test
+    public void unitTest3() {
+        Assertions.assertEquals(application.imageFormatDetection("images/imagenICO.ico"), FileType.ICO);
+    }
+
+    @Test
+    public void unitTest4() {
+        Assertions.assertEquals(application.imageFormatDetection("images/imagenGIF.gif"), FileType.GIF);
+    }
+}
+
+class TestsApps2{
+    App application = new App();
+    @Test
+    public void unitTest1(){
+        Assertions.assertEquals(application.imageFormatDetection("cadena incorrecta"),FileType.UNKNOWN);
+        System.out.println("Si el usuario introduce una cadena incorrecta, se le avisa con dos mensajes distintos");
+    }
 }
