1786b3fecbc24c1428c4662ecaaedb5ca428125e
Yves Andres <andreselianordam@mail.com>
2024-10-08 20:46:03 +0200

    update clase Martes

--- UNIT2/Activity711/src/Application.java
+++ UNIT2/Activity711/src/Application.java
@@ -1,36 +1,100 @@
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
+import javax.tools.FileObject;
+import java.io.*;
+import java.net.URI;
+import java.util.Scanner;
 
 public class Application {
 
     // Debugging purposes 'filePath'
-    private String filePath = "resources/contacts.obj";
-    private Reader fileRead = FileReader.nullReader();
+    private String resultFilePath = "resources/contacts.obj";
+    private Agenda agenda = new Agenda();
 
-    public void launch(){
-        readFile();
-        displayFileContent();
+
+    public void launch() {
+        UI.UIWelcome();
+        UI.UIMainPage();
+        UI.UISelection();
+
+        //loadAgenda();
+        //userSelectOption();
+
+        //Contact contact = agenda.createContact();
+        //serializeContact(contact);
+
+        viewAgenda();
     }
 
-    private void readFile(){
+    private void viewAgenda() {
         try {
-            fileRead = new FileReader(filePath);
-        }catch (FileNotFoundException e){
-            System.out.println("File not found");
+            ObjectInputStream view = new ObjectInputStream(new FileInputStream(resultFilePath));
+            Contact contact = (Contact) view.readObject();
+            System.out.println(contact.getName());
+        } catch (IOException e1) {
+            e1.getMessage();
+        } catch (ClassNotFoundException e2){
+            e2.getMessage();
+        }
+
+
+
+    }
+
+    private void serializeContact(Contact contact) {
+        try {
+            ObjectOutputStream agendaWrite = new ObjectOutputStream(new FileOutputStream(resultFilePath));
+            agendaWrite.writeObject(contact);
+        } catch (IOException e) {
+            e.getMessage();
         }
     }
 
-    private void displayFileContent(){
+    private void loadAgenda() {
         try {
-            int n = 0;
-            while(n != -1) {
-                n = fileRead.read();
-                System.out.print((char)n);
+            ObjectInputStream agendaFile = new ObjectInputStream(new FileInputStream(resultFilePath));
+            int numObjects = agendaFile.readInt();
+            for (int i = 0; numObjects > 0; numObjects--) {
+                Contact contact = (Contact) agendaFile.readObject();
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println("Error");
+        } catch (ClassNotFoundException e2) {
+            System.out.println("Error");
         }
+
+
     }
+
+    private void userSelectOption() {
+        Scanner scannerInput = new Scanner(System.in);
+        String selection = scannerInput.next();
+
+        switch (selection) {
+            case "A":
+                userAddContact();
+                break;
+            case "L":
+                userListContact();
+                break;
+            case "F":
+                userFindContact();
+                break;
+            default:
+                System.out.println("Insert a valid option");
+                break;
+        }
+        scannerInput.close();
+    }
+
+    private void userAddContact() {
+        agenda.createContact();
+    }
+
+    private void userListContact() {
+
+    }
+
+    private void userFindContact() {
+
+    }
+
 }
