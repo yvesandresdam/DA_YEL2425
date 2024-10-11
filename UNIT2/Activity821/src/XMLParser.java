26c530aecf21c698da08c0d5f8ee20582fc3601d
andreselianor <113841548+andreselianor@users.noreply.github.com>
2024-10-12 01:18:31 +0200

    update viernes

--- UNIT2/Activity821/src/XMLParser.java
+++ UNIT2/Activity821/src/XMLParser.java
@@ -4,9 +4,11 @@ import org.xml.sax.helpers.DefaultHandler;
 
 public class XMLParser extends DefaultHandler {
     protected String tagContent;
+    private String currentElement = "";
 
     // Opening TAG
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        currentElement = qName;
         if (qName.equals("contact")) {
             System.out.println("ID: " + attributes.getValue("id"));
         }
@@ -19,10 +21,15 @@ public class XMLParser extends DefaultHandler {
 
     // Ending TAG
     public void endElement(String uri, String localName, String qName) throws SAXException {
-        if (!qName.isBlank()) {
-            if (!qName.equals("contact")) {
-                System.out.println(" " + qName + ": " + tagContent);
-            }
+        if (qName.equals("home")) {
+            System.out.println("Home: " + tagContent); // Imprimir solo el contenido de home
+        } else if (qName.equals("cell")) {
+            System.out.println("Cell: " + tagContent);
+        } else {
+            System.out.println("Work: " + tagContent);
+        }
+            // Reiniciar el contenido para la siguiente etiqueta
+            tagContent = "";
+            currentElement = "";
         }
     }
-}
