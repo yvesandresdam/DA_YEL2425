70abca81695c15e458e78684a6303443d00f5aa4
andreselianor <113841548+andreselianor@users.noreply.github.com>
2024-10-12 21:32:09 +0200

    Activity 8.2.2
    Testing and reusing existing Code. Creation of a JAR document

---
+++ UNIT2/Activity822/Activity822/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Activity 8.2.2");
+        System.out.println("Parsing an XML");
+
+        Contact c = new Contact();
+        Agenda a = new Agenda();
+        a.addContact(c);
+    }
+}
