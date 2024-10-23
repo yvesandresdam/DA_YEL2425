fd03b29b7a91667b3fa58f7c8154b6b0800b44ca
Yves Andres <andreselianordam@mail.com>
2024-10-23 16:18:42 +0200

    updt clase Miercoles 23
    Activity 744 - calling Procedures with JAVA

--- UNIT3/Activity744/src/main/java/org/example/DBPC.java
+++ UNIT3/Activity744/src/main/java/org/example/DBPC.java
@@ -8,6 +8,8 @@ public class DBPC {
     private final static String userPass = "postgres";
     private final static String findByJob = "employees_find_by_job";
     private final static String findByDepartment = "employees_find_by_department";
+    private final static String findByName = "employees_find_by_name";
+    private final static String findByNameWildcard = "employees_find_by_name_bis";
 
     public void connectDB(){
         String URL = "jdbc:postgresql://localhost:5432/" + databaseName;
@@ -21,21 +23,37 @@ public class DBPC {
         }
     }
 
-    public void callingProcedures(int numberProcedure) {
-        String URL = "jdbc:postgresql://localhost:5432/" + databaseName;
+    public void callingProcedures(int numberProcedure, String wildcard) {
 
+        // Connecting the Database
+        String URL = "jdbc:postgresql://localhost:5432/" + databaseName;
         try (Connection connection = DriverManager.getConnection(URL, userName, userPass)) {
 
+            // User Procedure Selection
             String procedureName = "";
             if(numberProcedure == 1){
                 procedureName = String.format("{call %s('SALESMAN')}",findByJob);
             }
             else if(numberProcedure == 2){
-                procedureName = String.format("{call %d('10')}",findByDepartment);
+                procedureName = String.format("{call %s('20')}",findByDepartment);
+            }
+            else if(numberProcedure == 3){
+                procedureName = String.format("{call %s('A%%')}",findByName, "A%"); // Como curiosidad, el caracter % escapa con '%%'
+                //String procedureNameTest1 = String.format("{call %s('%s')}",findByName, "A%");  // Refactorizando
+                //String procedureNameTest2 = String.format("{call %s('%s')}",findByName, finder);  // Refactorizando
+            }
+            else if(numberProcedure == 4){
+                procedureName = String.format("{call %s('%s')}",findByNameWildcard, wildcard);  // Refactorizando
+            }
+            else{
+                System.out.println("Procedure number not valid");
             }
 
+            // Calling procedure with JAVA
             CallableStatement statement = connection.prepareCall(procedureName);
             ResultSet resultSet = statement.executeQuery();
+
+            // Displaying Data
             System.out.println("Code" + "\t" + "Name" + "\t" + "Job " + "\t\t" + "DPT");
             System.out.println("--------------------------------------------------");
             while (resultSet.next()) {
@@ -43,7 +61,7 @@ public class DBPC {
                 String column2 = resultSet.getString((2));
                 String column3 = resultSet.getString((3));
                 String column4 = resultSet.getString((4));
-                System.out.println(column1 + "\t" + column2 + "\t" + column3 + "\t" + column4);
+                System.out.println(column1 + "\t" + column2 + "\t" + column3 + "\t\t" + column4);
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
--- UNIT3/Activity744/src/main/java/org/example/Main.java
+++ UNIT3/Activity744/src/main/java/org/example/Main.java
@@ -6,17 +6,19 @@ public class Main {
         System.out.println("Calling Procedures");
         System.out.println();
 
-
         DBPC DBProceduresCalling = new DBPC();
 
         // Checking the DB Connection
         // DBProceduresCalling.connectDB();
 
         // Checking the Procedures list
-        // procedure n1 = findByJob         ||      ProcedureType.FIND_BY_JOB
-        // procedure n2 = findByDepartment  ||      ProcedureType.FIND_BY_DEPARTMENT
+        // procedure n1 = findByJob          ||      ProcedureType.FIND_BY_JOB
+        // procedure n2 = findByDepartment   ||      ProcedureType.FIND_BY_DEPARTMENT
+        // procedure n3 = findByName         ||      ProcedureType.FIND_BY_NAME
+        // procedure n4 = findByNameWildcard ||      ProcedureType.FIND_BY_NAME_WILDCARD
 
-        int numberPocedure = 1;
-        DBProceduresCalling.callingProcedures(numberPocedure);
+        int numberPocedure = 2;
+        String wildcard = "A%";
+        DBProceduresCalling.callingProcedures(numberPocedure, wildcard);
     }
 }
