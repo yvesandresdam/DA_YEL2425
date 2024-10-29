509c10f51a7ed3b4302076fa3cd816aac3cbe743
Yves Andres <andreselianordam@mail.com>
2024-10-29 20:43:57 +0100

    Activity 441
    Ejercicio en clase, Martes 29, proyecto hibernate

--- UNIT4/EjercicioInicial/EjercicioInicial/src/main/java/org/example/DeptEntity.java
+++ UNIT4/EjercicioInicial/EjercicioInicial/src/main/java/org/example/DeptEntity.java
@@ -1,8 +1,6 @@
 package org.example;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.util.List;
 
@@ -12,11 +10,26 @@ public class DeptEntity {
     @OneToMany(mappedBy = "department")
     private List<EmployeeEntity> employees;
 
+    @Id
+    @Column(name = "deptno")
+    private Long id;
+
+    @Column(name = "dname")
+    private String nombre;
+
     public List<EmployeeEntity> getEmployees() {
         return employees;
     }
 
     public void setEmployees(List<EmployeeEntity> employees) {
         this.employees = employees;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
--- UNIT4/EjercicioInicial/EjercicioInicial/src/main/java/org/example/Main.java
+++ UNIT4/EjercicioInicial/EjercicioInicial/src/main/java/org/example/Main.java
@@ -1,7 +1,30 @@
 package org.example;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hibernate Mapping Activity");
+
+        SessionFactory sessionFactory =
+                new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        if (session != null) {
+            System.out.println("Session successfully opened!");
+        } else {
+            System.out.println("Error opening session!");
+        }
+
+        Query<EmployeeEntity> myQuery =
+                session.createQuery("from org.example.EmployeeEntity");
+        List<EmployeeEntity> employees = myQuery.list();
+        for ( EmployeeEntity employee : employees ) {
+            System.out.printf("Number : %d Name: %s\n", employee.getEmpno(), employee.getEname());
+        }
     }
 }
