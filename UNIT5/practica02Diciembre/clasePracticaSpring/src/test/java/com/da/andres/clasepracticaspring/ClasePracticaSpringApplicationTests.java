4851344300863e643aa68e1f587070cce9a41078
Yves Andres <andreselianordam@mail.com>
2024-12-02 20:29:23 +0100

    clase lunes

---
+++ UNIT5/practica02Diciembre/clasePracticaSpring/src/test/java/com/da/andres/clasepracticaspring/ClasePracticaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.da.andres.clasepracticaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClasePracticaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
