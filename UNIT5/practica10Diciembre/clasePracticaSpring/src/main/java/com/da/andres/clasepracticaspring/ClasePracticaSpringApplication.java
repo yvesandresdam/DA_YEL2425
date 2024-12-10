b9bf53e28b8966695757e917c89177bd23e672df
Yves Andres <andreselianordam@mail.com>
2024-12-10 19:50:25 +0100

    final clase Martes 10 Diciembre

---
+++ UNIT5/practica10Diciembre/clasePracticaSpring/src/main/java/com/da/andres/clasepracticaspring/ClasePracticaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.da.andres.clasepracticaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClasePracticaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClasePracticaSpringApplication.class, args);
+    }
+
+}
