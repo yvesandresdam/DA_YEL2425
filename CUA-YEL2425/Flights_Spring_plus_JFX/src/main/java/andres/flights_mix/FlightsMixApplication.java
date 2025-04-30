4ed270530dd32c948b005a1a9955445ae5d09fc3
andreselianor <113841548+andreselianor@users.noreply.github.com>
2025-04-30 10:39:43 +0200

    update folder clean up

--- CUA-YEL2425/Flights_Spring_plus_JFX/src/main/java/andres/flights_mix/FlightsMixApplication.java
+++
@@ -1,9 +0,0 @@
-package andres.flights_mix;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-@SpringBootApplication
-public class FlightsMixApplication {
-    public static ConfigurableApplicationContext context;
-}
