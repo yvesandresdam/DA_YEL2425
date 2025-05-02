543db50eb46f72aa6c0856b11c896bde89771ad8
andreselianor <113841548+andreselianor@users.noreply.github.com>
2025-05-02 18:48:15 +0200

    corrected endpoint

--- CUA-YEL2425/Flights_v2/src/main/java/andres/flights_v2/controllersWEB/TicketsControllerWEB.java
+++ CUA-YEL2425/Flights_v2/src/main/java/andres/flights_v2/controllersWEB/TicketsControllerWEB.java
@@ -1,20 +1,35 @@
 package andres.flights_v2.controllersWEB;
 
 import andres.flights_v2.dto.TicketDTO;
+import andres.flights_v2.models.entities.AirportEntity;
+import andres.flights_v2.service.FlightService;
 import andres.flights_v2.service.TicketService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/Web/Tickets")
 public class TicketsControllerWEB {
     @Autowired
     private TicketService ticketService;
+    @Autowired
+    private FlightService flightService;
+
+    @GetMapping("/CreateTicket")
+    public String showCreateTicketForm(Model model) {
+        List<AirportEntity> origins = flightService.findAllOrigins();
+        model.addAttribute("origins", origins);
+        model.addAttribute("ticketDTO", new TicketDTO());
+        return "form_page";
+    }
 
     @PostMapping("/CreateTicket")
     public String createNewTicket(@Valid @ModelAttribute TicketDTO ticketDTO, Model model) {
